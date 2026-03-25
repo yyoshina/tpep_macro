b4e90b5f17cbb45cb2f1a3fa4f7156d44e8cb83d
Yusuke Yoshinaga <32575657+yyoshina@users.noreply.github.com>
2026-03-25 15:15:27 +0900

    Delete MusicModeStartOnCall.js

--- MusicModeStartOnCall.js
+++
@@ -1,9 +0,0 @@
-import xapi from 'xapi';
-
-function listenToCalls(){
-  xapi.event.CallSuccessful.on(async() => {
-    xapi.Command.Audio.Microphone.MusicMode.Start();
-  });
-}
-
-listenToCalls();
